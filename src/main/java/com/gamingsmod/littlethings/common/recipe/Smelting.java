20245d2c4f76623e8f1604c5a884d58296140d25
Dominic Carrington <carringtondominic@gmail.com>
2016-04-04 21:14:25 +0100

    Added smelting recipes for the other rails

--- src/main/java/com/gamingsmod/littlethings/common/recipe/Smelting.java
+++ src/main/java/com/gamingsmod/littlethings/common/recipe/Smelting.java
@@ -3,6 +3,7 @@ package com.gamingsmod.littlethings.common.recipe;
 import com.gamingsmod.littlethings.common.init.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -12,6 +13,9 @@ public class Smelting
     public static void init()
     {
         reg(Blocks.rail, new ItemStack(ModItems.IronNugget, 2), 0.3F);
+        reg(Blocks.golden_rail, new ItemStack(Items.gold_ingot), 0.3F);
+        reg(Blocks.activator_rail, new ItemStack(Items.iron_ingot), 0.3F);
+        reg(Blocks.detector_rail, new ItemStack(Items.iron_ingot), 0.3F);
     }
 
     private static void reg(Item input, ItemStack output, float xp)
