20acead686761fb54690d6cfc45d108ad894e4da
Dominic Carrington <carringtondominic@gmail.com>
2016-04-10 10:24:58 +0100

    Added workbenches to ore dictionary

--- src/main/java/com/gamingsmod/littlethings/common/init/ModOreDic.java
+++ src/main/java/com/gamingsmod/littlethings/common/init/ModOreDic.java
@@ -1,15 +1,24 @@
 package com.gamingsmod.littlethings.common.init;
 
+import com.gamingsmod.littlethings.common.block.BlockVanillaCraftingTables;
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class ModOreDic
 {
     public static void init()
     {
+        ItemStack[] workbenches = new ItemStack[1 + BlockVanillaCraftingTables.Variant.values().length];
+        workbenches[0] = new ItemStack(Blocks.crafting_table);
+        for (BlockVanillaCraftingTables.Variant variant : BlockVanillaCraftingTables.Variant.values())
+            workbenches[variant.getId() + 1] = new ItemStack(ModBlocks.VanillaCraftingTables, 1, variant.getId());
+
         add("nuggetIron", ModItems.IronNugget);
-    }
+        add("workbench", workbenches);
+;    }
 
     private static void add(String entry, Item... items)
     {
@@ -21,5 +30,11 @@ public class ModOreDic
     {
         for (Block block : blocks)
             OreDictionary.registerOre(entry, block);
+    }
+
+    private static void add(String entry, ItemStack... itemStacks)
+    {
+        for (ItemStack stack : itemStacks)
+            OreDictionary.registerOre(entry, stack);
     }
 }
