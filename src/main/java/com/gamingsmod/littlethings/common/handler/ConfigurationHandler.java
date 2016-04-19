19d3dde6017e197a83f765443d7c06210839263f
Dominic Carrington <carringtondominic@gmail.com>
2016-04-19 17:56:19 +0100

    Made config cleaner

--- src/main/java/com/gamingsmod/littlethings/common/handler/ConfigurationHandler.java
+++ src/main/java/com/gamingsmod/littlethings/common/handler/ConfigurationHandler.java
@@ -26,6 +26,11 @@ public class ConfigurationHandler
 
     public static double percentOfSkullSpawn;
 
+    public static final String CATEGORY_ITEMS = "Items";
+    public static final String CATEGORY_BLOCKS = "Blocks";
+    public static final String CATEGORY_RECIPES = "Recipes";
+    public static final String CATEGORY_HORSE_ARMOR = "Horse Armor Recipes";
+    
     public static void init(File file)
     {
         if (configuration == null) {
@@ -36,26 +41,61 @@ public class ConfigurationHandler
 
     public static void loadConfig()
     {
-        enableColorfulText = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Rude Flavour Text", false, "This is mainly for friend skull's flavour text").getBoolean();
-        enableExtraCraftingTables = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Extra Crafting Tables", true).getBoolean();
-        enableItemElevator = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Item Elevator", true).getBoolean();
-        enableSkullOwner = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Skull Owner", true).getBoolean();
-        enableUpgradedFurnaces = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Upgraded Furnaces", true).getBoolean();
-        enableGlassPanesRecipe = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Recoloring Glass Panes", true).getBoolean();
-        enableAnimalChests = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Animal Chests", true).getBoolean();
-        enableClearGlass = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Clear Glass", true).getBoolean();
-        enableHorseEquipment = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Horse Equipment Recipes", true).getBoolean();
-        enableUnenchantingTable = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Unenchanting Table", true).getBoolean();
-        enableHammers = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Hammers", true).getBoolean();
-        enableExcavators = configuration.get(Configuration.CATEGORY_GENERAL, "Enable Excavators", true).getBoolean();
+        //General
+        enableColorfulText = getBoolean(Configuration.CATEGORY_GENERAL, "Enable Rude Flavour Text", false, "This is mainly for friend skull's flavour text");
+        enableSkullOwner = getBoolean(Configuration.CATEGORY_GENERAL, "Enable Skull Owner", true);
+        percentOfSkullSpawn = getDouble(Configuration.CATEGORY_GENERAL, "Percent Of Zombie Spawning With Friend Skull", 0.01, 0.0, 1, "Higher the number the more common");
 
-        enableOldSaddleRecipe = configuration.get("Horse Equipment Recipes", "Enable Old Saddle Recipe", false, "Pre-13w18a Recipe").getBoolean();
-        enableOldArmorRecipe = configuration.get("Horse Equipment Recipes", "Enable Old Armor Recipe", false, "Pre-13w16a Recipe").getBoolean();
+        //Blocks
+        enableExtraCraftingTables = getBoolean(CATEGORY_BLOCKS, "Enable Extra Crafting Tables", true);
+        enableItemElevator = getBoolean(CATEGORY_BLOCKS, "Enable Item Elevator", true);
+        enableUpgradedFurnaces = getBoolean(CATEGORY_BLOCKS, "Enable Upgraded Furnaces", true);
+        enableAnimalChests = getBoolean(CATEGORY_BLOCKS, "Enable Animal Chests", true);
+        enableClearGlass = getBoolean(CATEGORY_BLOCKS, "Enable Clear Glass", true);
+        enableUnenchantingTable = getBoolean(CATEGORY_BLOCKS, "Enable Unenchanting Table", true);
 
-        percentOfSkullSpawn = configuration.get(Configuration.CATEGORY_GENERAL, "Percent Of Zombie Spawning With Friend Skull", 0.01, "Higher the number the more common", 0.0, 1).getDouble();
+        //Items
+        enableHammers = getBoolean(CATEGORY_ITEMS, "Enable Hammers", true);
+        enableExcavators = getBoolean(CATEGORY_ITEMS, "Enable Excavators", true);
+
+        //Recipes
+        enableGlassPanesRecipe = getBoolean(CATEGORY_RECIPES, "Enable Recoloring Glass Panes", true);
+        enableHorseEquipment = getBoolean(CATEGORY_RECIPES, "Enable Horse Equipment Recipes", true);
+        enableOldSaddleRecipe = getBoolean(CATEGORY_HORSE_ARMOR, "Enable Old Saddle Recipe", false, "Pre-13w18a Recipe");
+        enableOldArmorRecipe = getBoolean(CATEGORY_HORSE_ARMOR, "Enable Old Armor Recipe", false, "Pre-13w16a Recipe");
 
         if (configuration.hasChanged()) {
             configuration.save();
         }
+    }
+
+    private static boolean getBoolean(String cat, String key, boolean def, String desc)
+    {
+        return configuration.get(cat, key, def, desc).getBoolean();
+    }
+
+    private static boolean getBoolean(String cat, String key, boolean def)
+    {
+        return getBoolean(cat, key, def, null);
+    }
+
+    private static int getInteger(String cat, String key, int def, int min, int max, String desc)
+    {
+        return configuration.get(cat, key, def, desc, min, max).getInt();
+    }
+
+    private static int getInteger(String cat, String key, int def, int min, int max)
+    {
+        return getInteger(cat, key, def, min, max, null);
+    }
+
+    private static double getDouble(String cat, String key, double def, double min, double max, String desc)
+    {
+        return configuration.get(cat, key, def, desc, min, max).getDouble();
+    }
+
+    private static double getDouble(String cat, String key, double def, double min, double max)
+    {
+        return getDouble(cat, key, def, min, max, null);
     }
 }
